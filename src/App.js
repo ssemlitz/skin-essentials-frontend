581877e95e829e06a643c51198db959b6df3a166
ssemlitz <sdsemlitz@yahoo.com>
2023-03-22 15:32:13 -0400

    stub up hero banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import HeroBanner from './components/HeroBanner/HeroBanner';
 
 function App() {
   return (
     <div className="App">
-
+      <HeroBanner />
     </div>
   );
 }
--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -1,7 +1,18 @@
+import styles from './HeroBanner.module.css'
+import { Link } from 'react-router-dom';
+
 const HeroBanner = () => {
   return ( 
     <div>
-      <img src="" alt="" />
+      <img className={styles.heroImage} src="./images/hero-banner.jpg" alt="" />
+      <div>
+        <p>Have fun in the sun with our</p>
+        <h1>Summer Sale</h1>
+        <Link>
+          <button>Best Sellers</button>
+        </Link>
+      </div>
+      
     </div>
   );
 }
